78f3f967ef52dac142b0bd437e2835b7c883701b
Piotr Ujazdowski <aws-demo@MacBook-Pro-piotru.local>
2022-12-04 14:53:58 +0100

    Initial commit

---
+++ test/fake_tube.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FakeTube from '../lib/fake_tube-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fake_tube-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FakeTube.FakeTubeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
